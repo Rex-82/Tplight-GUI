a4b98a1a51ad6fa488fd66723432410a1c6b88cc
Simone Ferretti <simone.ferretti.work@gmail.com>
2023-09-16 20:01:03 +0200

    build(main): Add reloading
    Integrate electron-reloader in main.js to enable usage of the previously added package. Remove this for production.

--- main.js
+++ main.js
@@ -2,11 +2,15 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 400,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
